3988564330cc2e8c1fb4dcfa26a028aa8c521177
elmerastudillo <astudilloelmer@icloud.com>
2017-09-17 00:22:44 -0700

    Improved get random object function

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,22 +1,18 @@
 import random
-text_file = open("/usr/share/dict/words")
-# content = text_file.readlines()
-# print(content)
 
 # looping through each word and stripping the \n in filename by using rstrip()
 # Pseudocode
-# content =  words array
 # shuffle through content and return words by number user entered
-content = [word.rstrip('\n') for word in text_file]
-# print(content)
-number = int(input("Enter number:"))
-# content_range = range(int(number))
-# content_range[number] for i in content
-
 # loop the number of times the user inputs by using the range function
-for _ in range(number):
-    print(random.choice(content))
 
+def get_object_from_list(list, int):
+    for _ in range(int):
+        rand_index = random.randint(0, len(list) - 1)
+        value = content[rand_index]
+        print(value)
 
-
+text_file = open("/usr/share/dict/words")
+content = [word.rstrip('\n') for word in text_file]
+number = int(input("Enter number:"))
+get_object_from_list(content, number)
 text_file.close()
