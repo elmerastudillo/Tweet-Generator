a61a57bfdba7f14fee212226b554517eb0c7e8dc
elmerastudillo <astudilloelmer@icloud.com>
2017-09-24 16:46:34 -0700

    Running python script on Flask Server

--- server.py
+++ server.py
@@ -9,9 +9,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def random_word():
+    """Return random word using Flask Server."""
+    word_sentence = []
     file_path = "The_Journal_of_Prison_Discipline.txt"
     text_file = text_file_list(file_path)
     word_dictionary = histogram(text_file)
     dictionary = get_word_weight_dict(word_dictionary)
-    random_word = get_random_word_by_weight_prob(dictionary)
-    return random_word
+    for _ in range(1, 10):
+        random_word = get_random_word_by_weight_prob(dictionary)
+        word_sentence.append(random_word)
+    sentence_string = ' '.join(word_sentence)
+    return sentence_string
