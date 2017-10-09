16a80487f7e72c8c4ef76837fee6422732de22a5
elmerastudillo <astudilloelmer@icloud.com>
2017-10-08 23:52:29 -0700

    Implemented Markov Model

---
+++ Markov_Model.py
@@ -0,0 +1,19 @@
+from  import Dictogram
+
+
+def markov_chain(data):
+
+    markov_model = dict()
+
+    for i in range(0, len(data) - 1):
+        if data[i] in markov_model:
+            markov_model[data[i]].update(data + 1)
+        else:
+            markov_model[Dictogram(data[i + 1])]
+    return markov_model
+
+
+if __name__ == '__main__':
+    text_file = open('The_Journal_of_Prison_Discipline.txt')
+    text_list = [word.rstrip('/n') for word in text_file]
+    markov_chain(text_list)
